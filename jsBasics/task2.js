4b4bdfe431793dda2f30709f43b89bc7351f8e07
MariyaPasechnik <mariyapasechnik1992@gmail.com>
2025-11-15 18:09:40 +0200

    Add js basics homework

---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+const personOne = 'Alex';
+const personTwo = 'Xena';
+const greeting = `Hello, ${personOne} and ${personTwo}!`;
+const greetingConcat = 'Hello, ' + personOne + ' and ' + personTwo + '!';
+console.log(greeting);
+console.log(greetingConcat);
